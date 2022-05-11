fa51fb90bab262dc89391023aab0080931e64620
MiraJuwita <98795964+MiraJuwita@users.noreply.github.com>
2022-05-11 14:22:15 +0700

    Add files via upload

---
+++ G1A021056_MiraJuwitaAli_DrawingTriangle.cpp
@@ -0,0 +1,16 @@
+// algoritma
+
+int <- i, k,n;
+
+// for (i = 1; i <=n; i++)
+    i <- 1              
+ 	for  i <= n then
+ 	i++
+
+// for (k = 1; k <= i; k++)
+	k <- 1
+ 	for k<=i then
+ 	k++
+
+    cout <<"*";
+
